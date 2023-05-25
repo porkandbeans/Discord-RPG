214fd6047c7584544ec1df7d4fb85f2d69091241
RubbishPanda <30775997+porkandbeans@users.noreply.github.com>
2023-05-25 16:34:35 +1200

    buttsbot

---
+++ buttsbot.py
@@ -0,0 +1,25 @@
+import os
+import discord
+import random
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('BUTTSBOT_TOKEN')
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_message(message):
+    if random.randint(0,10) != 1:
+        return
+    
+    word_list = message.content.split()
+    random_index = random.randint(0, len(word_list) - 1)
+    word_list[random_index] = "butt"
+
+    new_string = " ".join(word_list)
+
+    await message.channel.send(new_string)
+
+client.run(TOKEN)
