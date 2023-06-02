e8068de2fb65a2238739d9d8a1cbf18115e915fe
RubbishPanda <30775997+porkandbeans@users.noreply.github.com>
2023-06-02 12:47:42 +1200

    gallery

--- webui/index.py
+++ webui/index.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask, render_template
+from PIL import Image
 app = Flask(__name__)
+
+imagedir = "D:\Projects\Discord-RPG\webui\static\images"
+file_list = [f for f in os.listdir(imagedir) if f.endswith('.jpeg')]
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', images=file_list)
 
 @app.route("/about")
 def about():
